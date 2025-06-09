62d7fad784b5aec8dd9f9d0d995d6e6efaae1eb9
stalin <info@gmail.com>
2025-06-09 11:34:52 +0530

    new file conversation

---
+++ 01converstaion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your name"
+read -s USERNAME
+echo "entered username is: $USERNAME"
+
+echo "Please enter your passcode"
+read -s PASSCODE
+echo "the passcode is: $PASSCODE"
+
